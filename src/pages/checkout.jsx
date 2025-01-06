f899157636be1900fc9e2cdec4b20b6eb1e44d9b
Venkatesh <venkateshdarapureddy268@gmail.com>
2025-01-06 14:55:37 +0530

    vercel

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -27,7 +27,7 @@ const CheckoutForm = () => {
         //`Elements` instance that was used to create the Payment Element
         elements,
         confirmParams: {
-          return_url: "https://example.com/order/123/complete",
+          return_url: "https://frontend-lemon-eight.vercel.app/",
         },
       });
   
